17e8bd3a8ace1f48801714866fdd38836059c847
cecchetti <marco.cecchetti.ext@sisal.it>
2024-04-11 10:19:58 +0200

    css carosello

--- src/app/features/components/carosello/carosello.component.ts
+++ src/app/features/components/carosello/carosello.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 import { CardComponent } from "../card/card.component";
 
 @Component({
@@ -6,9 +9,25 @@ import { CardComponent } from "../card/card.component";
     standalone: true,
     templateUrl: './carosello.component.html',
     styleUrl: './carosello.component.css',
-    imports: [CardComponent]
+    imports: [MatButtonModule, MatDividerModule, MatIconModule, CardComponent]
 })
 export class CaroselloComponent {
   cards = []
+  clicks = 800;
 
+
+
+  scrollRight(): void {
+    const parentElement = document.getElementsByClassName('container')[0];
+    parentElement!.scrollLeft += this.clicks; // Scroll to the specified position
+    this.clicks += 800; // Increment the scroll position
+  }
+
+  scrollLeft(): void {
+    const parentElement = document.getElementsByClassName('container')[0];
+    parentElement!.scrollLeft -= this.clicks; // Scroll to the specified position
+    this.clicks -= 800; // Increment the scroll position
+  }
 }
+
+
